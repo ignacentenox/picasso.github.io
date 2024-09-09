3acedab0aef110647c697bd3285040c205dcc732
Ignacio Manuel Centeno <ignacenteno46@gmail.com>
2024-09-08 21:24:00 -0300

    main

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ let currentVideo = 0;
 
 const videoElement = document.getElementById('playlist');
 
+// Función para activar el modo de pantalla completa
+function openFullscreen() {
+    if (videoElement.requestFullscreen) {
+        videoElement.requestFullscreen();
+    } else if (videoElement.mozRequestFullScreen) { // Firefox
+        videoElement.mozRequestFullScreen();
+    } else if (videoElement.webkitRequestFullscreen) { // Chrome, Safari and Opera
+        videoElement.webkitRequestFullscreen();
+    } else if (videoElement.msRequestFullscreen) { // IE/Edge
+        videoElement.msRequestFullscreen();
+    }
+}
+
 // Cargar el primer video
 videoElement.src = videos[currentVideo];
 
@@ -13,3 +26,9 @@ videoElement.addEventListener('ended', function () {
     videoElement.src = videos[currentVideo];
     videoElement.play(); // Reproduce el nuevo video
 });
+
+// Al hacer clic en el video, activar pantalla completa
+videoElement.addEventListener('click', openFullscreen);
+
+// Abrir pantalla completa automáticamente al cargar la página
+window.addEventListener('load', openFullscreen);
